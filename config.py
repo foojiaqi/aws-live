388934ec77b8f7142d1d5df71e5688392b2e097a
foojiaqi <112464623+foojiaqi@users.noreply.github.com>
2022-08-30 14:19:05 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cpzk4cq9m9x4.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "foojiaqi-employee"
+customregion = "us-east-1"
