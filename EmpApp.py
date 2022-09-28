3df41b967438452a2bdc8fdea74bf2627af5f6b6
foojiaqi <112464623+foojiaqi@users.noreply.github.com>
2022-09-29 01:01:55 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -100,17 +100,18 @@ def AddEmp():
         hourly_wage='0'
         hours_worked= '0'
         monthly_pay = '0'
+        check_in = '0'
         emp_image_file = request.files['emp_image_file']       
 
  
-        insert_sql = "INSERT INTO employee VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        insert_sql = "INSERT INTO employee VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
         cursor = db_conn.cursor()
 
         if emp_image_file.filename == "":
             return "Please select a file"
 
         try: 
-            cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location,leave_start_date,leave_end_date,leave_reason,leave_status,gender,job_title,date_of_hired,hourly_wage,hours_worked,monthly_pay))
+            cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location,leave_start_date,leave_end_date,leave_reason,leave_status,gender,job_title,date_of_hired,hourly_wage,hours_worked,monthly_pay,check_in))
             db_conn.commit()
             emp_name = "" + first_name + " " + last_name
             # Upload image file in S3 #
